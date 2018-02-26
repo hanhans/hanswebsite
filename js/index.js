e6de3c5bfafee6c80671e472f6bf41abc6ed1c1b
hanhans <jjwinds9@gmail.com>
2018-02-26 20:34:40 +0800

    upload index

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(window).scroll(function(e){
+  if($(window).scrollTop()<=0)
+    $(".explore,.navbar").addClass("at_top");
+  else
+    $(".explore,.navbar").removeClass("at_top");
+});
+
+
+$(document).on('click','a',function(event){
+  event.preventDefault();
+  var target=$(this).attr("href");
+  $('html,body').animate({
+    scrollTop: $(target).offset().top
+  },500);
+})
